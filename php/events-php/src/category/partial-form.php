10d6df9ea908414f469839bb4fbd1ca9ca50a0ff
fosseddy <markfosseddy@gmail.com>
2024-06-30 12:48:22 +0500

    rename

--- php/events-php/src/category/partial-form.php
+++
@@ -1,7 +0,0 @@
-<form method="POST">
-    <div class="form-field">
-        <input name="name" placeholder="name" value="<?= $name ?>">
-        <small><?= $errors["name"] ?? "&nbsp;" ?></small>
-    </div>
-    <button type="submit"><?= $button ?></button>
-</form>
